91ebf1b923d5399ab899e6bd72048be0fe4eef54
Omar Minaya <omiinaya@gmail.com>
2020-06-03 04:12:22 -0400

    added contact me page.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,15 +3,48 @@ import "./style.css";
 
 function Contact() {
     return (
-        <div className="contact">
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
-                        <p>Contact Page</p>
-                    </div>
+        <div>
+        <h1>.Contact Me</h1>
+        <section>
+          <div>
+            <p className="container-contact-text">If you like what you see and would like to collaborate with me on future projects, leave a message below!</p><p>
+            </p></div>
+        </section>
+        <section>
+          <div className="container-form">
+            <form action="/action_page.php">
+              <div className="row">
+                <div className="col-25">                            
+                  <label htmlFor="fname" style={{color: 'white'}}>First Name:</label>
                 </div>
-            </div>
-        </div>
+                <div className="col-75">
+                  <input type="text" id="fname" name="firstname" placeholder="Your name." />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-25">
+                  <label htmlFor="lname" style={{color: 'white'}}>Last Name:</label>
+                </div>
+                <div className="col-75">
+                  <input type="text" id="lname" name="lastname" placeholder="Your last name." />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-25">
+                  <label htmlFor="message" style={{color: 'white'}}>Message:</label>
+                </div>
+                <div className="col-75">
+                  <textarea id="message" name="message" placeholder="Write message here." style={{height: '200px'}} defaultValue={""} />
+                </div>
+              </div>
+              <div className="row">
+                <input type="submit" defaultValue="submit" />
+              </div>
+            </form>
+          </div>
+        </section>
+        <br />
+      </div>
     );
 }
 
