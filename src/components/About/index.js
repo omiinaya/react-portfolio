889feaca53e163f7e09dfb986176b07eaaedb2d0
Omar Minaya <omiinaya@gmail.com>
2020-06-03 03:58:43 -0400

    added portfolio page.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ function About() {
             <section>
                 <div className="container-about">
                     <div className="row">
-                        <div className="col-lg-3 col-md-3">
+                        <div className="col-lg-3 col-md-3 col-sm-3">
                             <img src={require('./images/omar.jpg')} alt="" />
                         </div>
                         <div className="col-lg-9 col-md-9 col-sm-9">
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,7 +3,45 @@ import "./style.css";
 
 function Portfolio() {
     return (
-        <div>test</div>
+        <div>
+        <h1>.Portfolio</h1>
+        <section>
+          <div className="container-portfolio">
+            <div className="row">
+              <div className="col-lg-3 col-md-4">
+                <a href="https://omiinaya.github.io/Project1/" target="_blank">
+                </a><article className="project"><a href="https://omiinaya.github.io/Project1/" target="_blank">
+                    <h2 className="title">Project 1</h2>
+                    <img src={require('./images/project1.png')} height="200px" width="200px" alt="" /></a>
+                </article>
+              </div>
+              <div className="col-lg-3 col-md-4">
+                <a href="https://omiinaya.github.io/pwd-gen/" target="_blank">
+                </a><article className="project"><a href="https://omiinaya.github.io/pwd-gen/" target="_blank">
+                    <h2 className="title">Password Generator</h2>
+                    <img src={require('./images/pwdgen.png')} height="200px" width="200px" alt="" /></a>
+                </article>
+              </div>
+              <div className="col-lg-3 col-md-4">
+                <a href="https://omiinaya.github.io/timed-quiz/" target="_blank">
+                </a><article className="project"><a href="https://omiinaya.github.io/timed-quiz/" target="_blank">
+                    <h2 className="title">Timed Quiz</h2>
+                    <img src={require('./images/timedquiz.png')} height="200px" width="200px" alt="" /></a>
+                </article>
+              </div>
+              <div className="col-lg-3 col-md-4">
+                <a href="https://omiinaya.github.io/day-planner" target="_blank">
+                </a><article className="project"><a href="https://omiinaya.github.io/day-planner" target="_blank">
+                    <h2 className="title">Day Planner</h2>
+                    <img src={require('./images/dayplanner.png')} height="200px" width="200px" alt="" /></a>
+                </article>
+              </div>
+              <div>
+              </div>
+            </div></div></section>
+        <br />
+        <section>
+        </section></div>
     );
 }
 
