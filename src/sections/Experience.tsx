f66f869b0f0fff8502c208af3af7c505dcb91c74
Omar <omiinaya@gmail.com>
2025-08-26 01:20:58 -0400

    replaced words with icons

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -10,20 +10,22 @@ const Experience: React.FC = () => {
     <section id="experience" className="experience section">
       <div className="container">
         <h2 className="section-title">{t('experience.title')}</h2>
-        <div className="experience-list">
-         {profile.sections.experience.items.map((exp) => (
-           <div key={exp.id} className="experience-item card">
-             <div className="experience-header">
-               <h3 className="experience-company">{exp.company}</h3>
-               <span className="experience-period">{exp.date}</span>
+        <div className="experience-content">
+          <div className="experience-list">
+           {profile.sections.experience.items.map((exp) => (
+             <div key={exp.id} className="experience-item card">
+               <div className="experience-header">
+                 <h3 className="experience-company">{exp.company}</h3>
+                 <span className="experience-period">{exp.date}</span>
+               </div>
+               <h4 className="experience-position">{exp.position}</h4>
+               <div
+                 className="experience-description"
+                 dangerouslySetInnerHTML={{ __html: exp.summary }}
+               />
              </div>
-             <h4 className="experience-position">{exp.position}</h4>
-             <div
-               className="experience-description"
-               dangerouslySetInnerHTML={{ __html: exp.summary }}
-             />
-           </div>
-         ))}
+           ))}
+          </div>
         </div>
       </div>
     </section>
